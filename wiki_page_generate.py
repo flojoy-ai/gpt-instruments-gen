961df005df59606172e47a7fd07ff73e4c0dedf8
Jinwon Lee <jinwonlee0916@gmail.com>
2023-08-22 18:04:46 -0400

    Update instruments cell

--- wiki_page_generate.py
+++ wiki_page_generate.py
@@ -1,13 +1,6 @@
-import numpy as np
-from load_data import Cols, load_data
 from generate_snippets import LIBRARY_SNIPPET_MAP
-from tqdm.auto import tqdm
-import requests
-import re
 import os
-from templates import DOCS_DIR, DOCS_TEMPLATE
-from data_utils import generate_url_slug, cloudinary_upload, clean_name
-
+from templates import DOCS_TEMPLATE
 
 def instruments_wiki(unique_libs, device_file, site_url, desc, device, vendor, device_img_url, vendor_desc, vendor_web, vendor_logo_url, headquarters, revenue, device_doc_dir, category):
     # gather all existing snippet directories for device
@@ -68,72 +61,3 @@ def instruments_wiki(unique_libs, device_file, site_url, desc, device, vendor, d
 
     with open(device_doc_file, "w") as fw:
         fw.write(device_doc)
-
-    print(f"Completed: {device_file} {category}")
-
-def category_instruments_wiki(category, category_included, category_description, current_category_devices, site_url, device_img_url, device, number, WIKI_PAGE_TEMPLATE, CATEGORY_TEMPLATE, NEW_CATEGORY_TEMPLATE):
-    if category not in category_included:
-        if number == 0:
-            WIKI_PAGE_TEMPLATE += f'## {category} \n\n <details> \n <summary>Category Description</summary> \n {category_description} \n </details> \n\n <div className="flex flex-wrap" style={{{{ marginLeft: "-55px" }}}}>\n\n\n<div className="p-4">\n\n<a href="{site_url}">\n<figure style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device}</figcaption>\n</figure>\n</a></div>'
-            CATEGORY_TEMPLATE += f'## {category} \n\n <details> \n <summary>Category Description</summary> \n {category_description} \n </details> \n\n <div className="flex flex-wrap" style={{{{ marginLeft: "-55px" }}}}>\n\n\n<div className="p-4">\n\n<a href="{site_url}">\n<figure style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device}</figcaption>\n</figure>\n</a></div>'
-
-        else:
-            current_category_devices.clear()
-
-            cat_url = (
-                category_included[-1]
-                .replace(" ", "-")
-                .replace("/", "-")
-                .replace("_", "-")
-                .lower()
-            )
-            page_url = f"/instruments-wiki/{cat_url}/"
-            category_docs = CATEGORY_TEMPLATE.format(
-                category=category_included[-1], page_url=page_url
-            )
-            category_docs = category_docs.replace("{", "{{").replace("}", "}}")
-            with open(
-                f"./docusaurus/{category_included[-1]}/{category_included[-1]}.md", "w"
-            ) as file:
-                file.write(category_docs + "\n</div>")
-
-            WIKI_PAGE_TEMPLATE += f'\n</div> \n\n ## {category} \n\n <details> \n <summary>Category Description</summary> \n {category_description} \n </details> \n\n <div className="flex flex-wrap" style={{{{ marginLeft: "-55px" }}}}>\n\n\n<div className="p-4">\n\n<a href="{site_url}">\n<figure style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device}</figcaption>\n</figure>\n</a></div>'
-
-            CATEGORY_TEMPLATE = NEW_CATEGORY_TEMPLATE
-            CATEGORY_TEMPLATE += f'## {category} \n\n <details> \n <summary>Category Description</summary> \n {category_description} \n </details> \n\n <div className="flex flex-wrap" style={{{{ marginLeft: "-55px" }}}}>\n\n\n<div className="p-4">\n\n<a href="{site_url}">\n<figure style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device}</figcaption>\n</figure>\n</a></div>'
-
-        category_included.append(category)
-    else:
-        if device not in current_category_devices:
-            WIKI_PAGE_TEMPLATE += f'\n\n\n<div className="p-4">\n\n<a href="{site_url}">\n<figure style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device}</figcaption>\n</figure>\n</a></div>'
-            CATEGORY_TEMPLATE += f'\n\n\n<div className="p-4">\n\n<a href="{site_url}">\n<figure style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "200px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device}</figcaption>\n</figure>\n</a></div>'
-
-    current_category_devices.append(device)
-
-    print(category + " " + str(number))
-    number += 1
-
-# def python_lib_wiki(lib: str, python_lib_dict: dict[str:str]):
-
-#         if previous_lib is None:
-#             previous_lib = lib
-
-#         if previous_lib != lib:
-#             PYTHON_LIB_TEMPLATE += "\n</div>"
-#             previous_lib = lib
-
-#         if lib not in python_lib_dict:
-#             file_path = os.path.join(LIBRARY_SNIPPET_MAP[lib], f"{lib}_description.txt")
-
-#         if not os.path.exists(file_path):
-#             continue
-        
-#         with open(file_path, "r") as f:
-#             python_lib_dict[lib] = f.read()
-
-#         PYTHON_LIB_TEMPLATE += f"\n\n## {lib} \n\n <details> \n <summary>Python Library Description</summary> \n {python_lib_dict[lib]} \n </details> \n\n"
-#         PYTHON_LIB_TEMPLATE += (
-#             f'<div className="flex flex-wrap" style={{{{ marginLeft: "-40px" }}}}>\n'
-#         )
-
-#     PYTHON_LIB_TEMPLATE += f'\n\n<div className="p-4">\n\n<a href="{docs_url}">\n\n<figure style={{{{ width: "185px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}}>\n<img src="{device_img_url}" style={{{{ width: "185px", height: "200px", objectFit: "scale-down", marginRight: "15px" }}}} />\n<figcaption>{device_name}</figcaption>\n</figure>\n</a>\n\n</div>'
